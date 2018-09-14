de9f1de2d5b0853c441db73b30de477c0fc46f9c
Ben Stone <bstone@shopkeep.com>
2018-09-13 21:33:28 -0400

    code cleanup

--- content.js
+++ content.js
@@ -1,65 +1,83 @@
 chrome.storage.sync.get('ShaqtivateEnabled', function(result) {
-  var status = result.ShaqtivateEnabled
-  if (status === "enabled") {
-    var elements = document.getElementsByTagName('*');
-
-    for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
-
-        for (var j = 0; j < element.childNodes.length; j++) {
-            var node = element.childNodes[j];
-
-            if (node.nodeType === 3) {
-                var fullNodeText = node.nodeValue;
-                var ShaqtivatingIsEnabled
-
-                if (fullNodeText.indexOf("}") !== -1 || fullNodeText.indexOf("\\") !== -1) {
-                  break
-                }
-
-                var words = fullNodeText.split(" ")
-                var finalWords = []
-
-                for (wordIndex = 0; wordIndex < words.length; wordIndex++) {
-                  var text = words[wordIndex]
-                  var skippableCharSequences = ["ace", "ach", "aci", "acy"]
-                  var shouldReplace = true
-                  for (k = 0; k < skippableCharSequences.length; k++) {
-                    var sequence = skippableCharSequences[k]
-                    var sequenceIndex = text.indexOf(sequence)
-                    if (sequenceIndex !== -1) {
-                      shouldReplace = false
-                    }
-                  }
-
-                  var shaqedText = text.replace("ac", "Shaq")
-                  shaqedText = shaqedText.replace("Ac", "Shaq")
-                  shaqedText = shaqedText.replace("hak","Shaq")
-
-                  var shaqIndex = shaqedText.indexOf("Shaq")
-
-                  var shouldTruncateBeginning = true
-
-
-                  for (letterIndex = 0; letterIndex < shaqIndex; letterIndex++) {
-                    var letter = shaqedText.charAt(letterIndex).toLowerCase()
-                    if (letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u") {
-                      shouldTruncateBeginning = false
-                    }
-                  }
-
-                  if (shouldTruncateBeginning) {
-                    shaqedText = shaqedText.substring(shaqIndex, shaqedText.length)
-                  }
-
-                  if (shouldReplace) {
-                    finalWords.push(shaqedText)
-                  }
-                }
-                var newText = finalWords.join(" ")
-                element.replaceChild(document.createTextNode(newText), node);
-            }
-        }
-    }
+  var isEnabled = result.ShaqtivateEnabled
+  if (isEnabled) {
+    shaqifyAllDOMText()
   }
 });
+
+function shaqifyAllDOMText() {
+  var elements = document.getElementsByTagName('*');
+  for (var i = 0; i < elements.length; i++) {
+      var element = elements[i]
+      for (var j = 0; j < element.childNodes.length; j++) {
+          var node = element.childNodes[j]
+          if (node.nodeType === 3) {
+              var newText = createShaqifiedElement(node)
+              element.replaceChild(document.createTextNode(newText), node);
+          }
+      }
+  }
+}
+
+function createShaqifiedElement(node) {
+  var fullNodeText = node.nodeValue
+  if (isUnshaqable(fullNodeText)) {
+    return node.nodeValue
+  }
+  var words = fullNodeText.split(" ")
+  var finalWords = []
+
+  for (wordIndex = 0; wordIndex < words.length; wordIndex++) {
+    finalWords.push(shaqify(words[wordIndex]))
+  }
+  return finalWords.join(" ")
+}
+
+function isUnshaqable(text) {
+  var codeIndicativeChars = ["}", "\\", "href"]
+  for (index = 0; index < codeIndicativeChars.length; index++) {
+    if (text.indexOf(codeIndicativeChars[index]) !== -1) {
+      return true
+    }
+  }
+  return false
+}
+
+function shaqify(text) {
+  if (containsSkippableSequence(text)) {
+    return text
+  }
+  var newText = text
+  var replacableSequences = ["ac", "Ac", "aC", "hak", "Hak"]
+  for (index = 0; index < replacableSequences.length; index++) {
+    var sequence = replacableSequences[index]
+    newText = newText.replace(sequence, "Shaq")
+  }
+  var shaqIndex = newText.indexOf("Shaq")
+  if (shouldTruncateBeginning(shaqIndex, newText)) {
+    newText = newText.substring(shaqIndex, newText.length)
+  }
+  return newText
+}
+
+function containsSkippableSequence(text) {
+  var skippableCharSequences = ["ace", "ach", "aci", "acy"]
+  for (index = 0; index < skippableCharSequences.length; index++) {
+    var sequence = skippableCharSequences[index]
+    var sequenceIndex = text.indexOf(sequence)
+    if (sequenceIndex !== -1) {
+      return true
+    }
+  }
+  return false
+}
+
+function shouldTruncateBeginning(shaqIndex, text) {
+  for (letterIndex = 0; letterIndex < shaqIndex; letterIndex++) {
+    var letter = text.charAt(letterIndex).toLowerCase()
+    if (letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u") {
+      return false
+    }
+  }
+  return true
+}
--- script.js
+++ script.js
@@ -1,44 +1,27 @@
 window.addEventListener('load', function load(event) {
-
-  var currentValue;
-
-  chrome.storage.sync.get({'ShaqtivateEnabled': ''}, function(result) {
-    var status = result.ShaqtivateEnabled
-    if (status === 'enabled') {
-      currentValue = 'enabled'
-      document.getElementById('ShaqtivateToggle').checked = true
-    }
-    if (status === 'disabled') {
-      currentValue = 'disabled'
-      document.getElementById('ShaqtivateToggle').checked = false
-    }
-  });
-
-    document.getElementById('ShaqtivateToggle').onclick = function() {
-
-      // window.open(`mailto:""?subject=""&body=""`);
-
-      chrome.storage.sync.get('ShaqtivateEnabled', function(result) {
-        var newValue
-        var status = result.ShaqtivateEnabled
-        // alert("The value currently is set to " + currentValue)
-        if (status === "enabled") {
-          newValue = "disabled" }
-        else if (status === "disabled") {
-          newValue = "enabled"
-        }
-        else {
-          newValue = "enabled"
-        }
-        // alert("the new value is " + newValue)
-        chrome.storage.sync.set({'ShaqtivateEnabled': newValue}, function() {
-          if (chrome.runtime.error) {
-            alert("Runtime error.");
-          }
-          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-              chrome.tabs.update(tabs[0].id, {url: tabs[0].url});
-          });
-        })
-      })
+  setInitialToggleState()
+  document.getElementById('ShaqtivateToggle').onclick = function() {
+    handleClick()
     }
 })
+
+function setInitialToggleState() {
+  chrome.storage.sync.get({'ShaqtivateEnabled': ''}, function(result) {
+    document.getElementById('ShaqtivateToggle').checked = result.ShaqtivateEnabled
+  });
+}
+
+function handleClick() {
+  chrome.storage.sync.get('ShaqtivateEnabled', function(result) {
+    var status = result.ShaqtivateEnabled
+    chrome.storage.sync.set({'ShaqtivateEnabled': !result.ShaqtivateEnabled}, function() {
+      refreshTab()
+    })
+  })
+}
+
+function refreshTab() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.update(tabs[0].id, {url: tabs[0].url});
+  })
+}
